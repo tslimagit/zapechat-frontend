12db03be365403db88889a4ffc0a6d0aca42c429
Thiago Lima <111473685+tslimagit@users.noreply.github.com>
2026-04-13 23:16:57 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Analytics } from "@vercel/analytics/react";
+{/* Speed */}
 import { SpeedInsights } from "@vercel/speed-insights/react";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
